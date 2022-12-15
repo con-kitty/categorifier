72ecfae4cfc564267f13a35f56a3b1d71d5c283c
Greg Pfeil <greg@technomadic.org>
2022-12-15 02:53:58 -0700

    Add a formatter

--- flake.nix
+++ flake.nix
@@ -1,42 +1,48 @@
 {
   description = "categorifier";
 
-  outputs = { self, nixpkgs, flake-utils, concat, linear-base }: let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    concat,
+    linear-base,
+  }: let
     supportedGhcVersions = ["ghc884" "ghc8107" "ghc902" "ghc924" "ghcHEAD"];
     dependencies =
       nixpkgs.lib.composeExtensions
-        concat.overlays.default
-        (final:
-          concat.lib.overlayHaskellPackages
-            supportedGhcVersions
-            (ghcVer: hfinal: hprev: let
-              haskellLib = final.haskell.lib;
-            in {
-              # test is broken for some GHC versions.
-              "PyF" = haskellLib.dontCheck hprev.PyF;
-              # test is broken.
-              "barbies" = haskellLib.dontCheck hprev.barbies;
-              # found the test is flaky.
-              "hls-pragmas-plugin" =
-                haskellLib.dontCheck hprev.hls-pragmas-plugin;
-              # linear-base 0.2.0
-              "linear-base" =
-                hfinal.callCabal2nix "linear-base" linear-base { };
-            })
-            final);
+      concat.overlays.default
+      (final:
+        concat.lib.overlayHaskellPackages
+        supportedGhcVersions
+        (ghcVer: hfinal: hprev: let
+          haskellLib = final.haskell.lib;
+        in {
+          # test is broken for some GHC versions.
+          "PyF" = haskellLib.dontCheck hprev.PyF;
+          # test is broken.
+          "barbies" = haskellLib.dontCheck hprev.barbies;
+          # found the test is flaky.
+          "hls-pragmas-plugin" =
+            haskellLib.dontCheck hprev.hls-pragmas-plugin;
+          # linear-base 0.2.0
+          "linear-base" =
+            hfinal.callCabal2nix "linear-base" linear-base {};
+        })
+        final);
     cabalPackages = concat.lib.cabalProject2nix ./cabal.project;
-  in {
-    overlays.default =
-      nixpkgs.lib.composeExtensions
+  in
+    {
+      overlays.default =
+        nixpkgs.lib.composeExtensions
         dependencies
         (final:
           concat.lib.overlayHaskellPackages
-            supportedGhcVersions
-            (ghcVer: hfinal: hprev: cabalPackages final ghcVer)
-            final);
-  }
-  // flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
-    let
+          supportedGhcVersions
+          (ghcVer: hfinal: hprev: cabalPackages final ghcVer)
+          final);
+    }
+    // flake-utils.lib.eachSystem flake-utils.lib.allSystems (system: let
       pkgs = import nixpkgs {
         inherit system;
         # NB: This uses `self.overlays.default` instead of `dependencies`
@@ -47,15 +53,17 @@
     in {
       # This package set is only useful for CI build test.
       # In practice, users will create a development environment composed by overlays.
-      packages = 
-        { default = self.packages.${system}.ghc902_all; }
+      packages =
+        {default = self.packages.${system}.ghc902_all;}
         // concat.lib.mkPackages pkgs supportedGhcVersions cabalPackages;
-      
+
       devShells =
-        { default = self.devShells.${system}.ghc902; }
+        {default = self.devShells.${system}.ghc902;}
         // concat.lib.mkDevShells pkgs supportedGhcVersions cabalPackages;
+
+      formatter = pkgs.alejandra;
     });
-  
+
   inputs = {
     concat = {
       inputs = {
@@ -64,14 +72,14 @@
       };
       url = github:con-kitty/concat/updated-flake;
     };
-    
+
     flake-utils.url = github:numtide/flake-utils;
-    
+
     linear-base = {
       flake = false;
       url = github:tweag/linear-base/v0.2.0;
     };
-    
+
     nixpkgs.url = github:NixOS/nixpkgs/release-22.11;
   };
 }
