afba707924fa09f2e42f8f2a8661fde53e4bef16
Greg Pfeil <greg@technomadic.org>
2024-02-08 15:34:08 -0700

    Better support for `concat-examples` categories
    This removes some duplicated instances, moves some others out of test-specific code, and adds a few
    others. Overall, this makes `ConCat.Circuit` and `ConCat.Syntactic` better supported under Categorifier.

--- integrations/concat/examples/Categorifier/ConCat/Examples/Syntactic.hs
+++ integrations/concat/examples/Categorifier/ConCat/Examples/Syntactic.hs
@@ -3,6 +3,8 @@
 
 -- | This is intended to be used /instead/ of "ConCat.Syntactic" when using the "Categorifier"
 --   plugin.
+--
+--   It re-exports the original module, adding instances for classes required by "Categorifier"..
 module Categorifier.ConCat.Examples.Syntactic
   ( module ConCat.Syntactic,
   )
@@ -10,14 +12,25 @@ where
 
 import qualified Categorifier.Category as Categorifier
 import qualified Categorifier.Client as Categorifier
+import qualified ConCat.Category as ConCat
 import ConCat.Syntactic
 
-instance
-  (Categorifier.HasRep a, r ~ Categorifier.Rep a, T a, T r) =>
-  Categorifier.RepCat Syn a r
-  where
+instance Categorifier.ReferenceCat Syn a b
+
+instance (Categorifier.HasRep a, r ~ Categorifier.Rep a) => Categorifier.RepCat Syn a r where
   abstC = app0' "abst"
   reprC = app0' "repr"
 
 instance Categorifier.UnsafeCoerceCat Syn a b where
   unsafeCoerceK = app0' "unsafeCoerce"
+
+-- TODO: Move these instances upstream
+
+instance (Functor f) => ConCat.Strong Syn f where
+  strength = app0 "strength"
+
+instance ConCat.TraversableCat Syn t f where
+  sequenceAC = app0 "sequenceA"
+
+instance ConCat.TracedCat Syn where
+  trace = app1 "trace"
